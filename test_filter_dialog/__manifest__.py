54e8b98b49fc514aa64714884b5bec919996a0ed
amiralserge <kossononserge@yahoo.fr>
2018-04-06 14:08:40 +0800

    new module

---
+++ test_filter_dialog/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Cotong Development Project Management',
+    'version': '1.0',
+    'category': 'project',
+    'summary': '开发项目管理模块',
+    'description': '',
+    'author': 'Shanghai Cotong Software Co., Ltd.',
+    'website': 'https://www.80sERP.com',
+    'depends': [
+        'project','website'
+    ],
+    'data': [
+        "views/project_task_form_view.xml",
+    ],
+    'installable': True,
+}
+
+
+
+
